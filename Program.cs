74b186f1718f9814a2d95ba907bdfeb163e81e40
Erlimar Silva Campos <erlimar@gmail.com>
2015-08-11 11:59:52 -0300

    Versão 7

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 using static System.Console;
+using Microsoft.Framework.Runtime;
 
 public class Program
 {
-	public Program()
+	public Program(IApplicationEnvironment env)
 	{
 		WriteLine("Class Program created!");
+		WriteLine(env.ApplicationName);
+		WriteLine(env.Version);
+		WriteLine(env.RuntimeFramework.FullName);
 	}
 	
 	public void Main()
