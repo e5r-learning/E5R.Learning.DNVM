3fece309377ea445ec42da3c33f0d0bab478e3dd
Erlimar Silva Campos <erlimar@gmail.com>
2015-08-11 11:31:24 -0300

    Versão 6

--- Program.cs
+++ Program.cs
@@ -2,6 +2,11 @@ using static System.Console;
 
 public class Program
 {
+	public Program()
+	{
+		WriteLine("Class Program created!");
+	}
+	
 	public void Main()
 	{
 		WriteLine("Hello .NET World!");
